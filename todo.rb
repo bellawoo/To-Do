f3d993571316d712a55486771d3ee4d10bd1dfae
Bella <woo.bella@gmail.com>
2015-06-05 14:47:03 -0400

    set up outline of script

---
+++ todo.rb
@@ -0,0 +1,35 @@
+def add_item
+	
+end
+
+def set_due_date
+	
+end
+
+def done?
+	
+end
+
+def view_incomplete_items
+	
+end
+
+def view_single_list
+	
+end
+
+def view_all_items
+	
+end
+
+def next_item
+	
+end
+
+def search_for_item
+	
+end
+
+def user_login
+	
+end
